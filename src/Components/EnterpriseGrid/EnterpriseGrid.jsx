62fd8657de15bbd93b2e630865fb6f72c8410654
João Pedro <joao_pedro_mattos@hotmail.com>
2019-08-24 20:19:03 -0300

    Fixed /enterprises bug... now the project is working properly...

--- src/Components/EnterpriseGrid/EnterpriseGrid.jsx
+++ src/Components/EnterpriseGrid/EnterpriseGrid.jsx
@@ -13,7 +13,7 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";  
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -69,35 +69,35 @@ export default function NestedGrid() {
           />
         </FormControl>
       </div>
-    </div>
 
-<Card className={classes.card}>
-<CardActionArea>
-  <CardMedia
-    component="img"
-    alt="Contemplative Reptile"
-    height="140"
-    image="/static/images/cards/contemplative-reptile.jpg"
-    title="Contemplative Reptile"
-  />
-  <CardContent>
-    <Typography gutterBottom variant="h5" component="h2">
-      Lizard
-    </Typography>
-    <Typography variant="body2" color="textSecondary" component="p">
-      Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-      across all continents except Antarctica
-    </Typography>
-  </CardContent>
-</CardActionArea>
-<CardActions>
-  <Button size="small" color="primary">
-    Share
-  </Button>
-  <Button size="small" color="primary">
-    Learn More
-  </Button>
-</CardActions>
-</Card>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
